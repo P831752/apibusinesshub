1bb3b19309fbdec3549d0f7931f87c1461ab8303
Venkata Ramireddy <sap.ramireddy@gmail.com>
2024-10-24 09:49:25 +0000

    working as video

---
+++ businesspartner/db/schema.cds
@@ -0,0 +1,7 @@
+namespace businesspartner;
+
+entity Episodes {
+    Key ID: String @title : 'Youtube Video ID';
+    title: String @title : 'Episode Title';
+}
+
---
+++ businesspartner/srv/show.js
@@ -0,0 +1,11 @@
+module.exports = (async function() {
+    const cds = require('@sap/cds')
+    const bp = await cds.connect.to('API_BUSINESS_PARTNER');
+    // need to install, npm i @sap-cloud-sdk/http-client
+    // data should come from differnt host (remote server) 38185 and it should be run mode
+
+    this.on('READ', 'Participant', (req) => {
+        console.log('Passing to remote system');
+        return bp.run(req.query);
+    })
+})
